7f572d1fb556183821914a6c72f8d45ebe4611e0
Kasia <katarzynadudziak94@gmail.com>
2025-01-29 12:16:02 +0100

    Added implementation Pawn's class methods: move and capture

--- pawns.py
+++ pawns.py
@@ -1,17 +1,29 @@
 class Pawn:
     def __init__(self, color, x, y):
         self.color = color
-        self.x = x
-        self.y = y
+        self.x = x   # The row: from 0 to 7
+        self.y = y   # The column: from 0 to 7
 
     def __str__(self):
         return f"{self.color} {self.__class__.__name__} at {self.x}{self.y}"
 
     def move(self, x, y):
-        pass
+        if self.color == "white":
+            if (x == self.x and y == self.y + 1) or (x == self.x and y == self.y + 2):
+                return True     # The pawn is moving one or two squares
+        elif self.color == "black":
+            if (x == self.x and y == self.y - 1) or (x == 6 and y == self.y - 2):
+                return True     # The pawn is moving one or two squares
+        return  False    # The pawn is not moving, moving more than two squares or moving in the wrong direction
 
     def capture(self, x, y):
-        pass
+        if self.color == "white":
+            if (x == self.x + 1 and y == self.y + 1) or (x == self.x + 1 and y == self.y - 1):
+                return True     # The pawn is capturing another pawn
+        elif self.color == "black":
+            if (x == self.x - 1 and y == self.y + 1) or (x == self.x - 1 and y == self.y - 1):
+                return True     # The pawn is capturing another pawn
+        return False    # The pawn is not capturing or capturing in the wrong direction
 
 
 class Bishop(Pawn):
@@ -69,12 +81,12 @@ class King(Pawn):
         pass
 
 
-pawn = Pawn("white", "H", 1)
-bishop = Bishop("black", "A", 1)
-rook = Rook("white", "D", 1)
-knight = Knight("black", "F", 1)
-queen = Queen("white", "D", 1)
-king = King("black", "A", 1)
+pawn = Pawn("white", 0, 1)
+bishop = Bishop("black", 2, 1)
+rook = Rook("white", 2, 1)
+knight = Knight("black", 5, 1)
+queen = Queen("white", 3, 1)
+king = King("black", 6, 1)
 
 print(pawn)
 print(bishop)
