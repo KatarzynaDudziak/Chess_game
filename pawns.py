a1b85d1821af589a19b1b449256c5924e8b7db51
Kasia <katarzynadudziak94@gmail.com>
2025-02-04 15:47:17 +0100

    Added typing

--- pawns.py
+++ pawns.py
@@ -1,14 +1,16 @@
+from point import Point
+
 class Pawn:
-    def __init__(self, color):
+    def __init__(self, color: str):
         self.color = color
 
     def __str__(self):
         return f"{self.color} {self.__class__.__name__}"
 
-    def can_move(self, current_pos, new_pos):
+    def can_move(self, current_pos: Point, new_pos: Point) -> bool:
         return is_moving_forward_one_square(self.color, current_pos, new_pos) or is_moving_forward_two_squares(self.color, current_pos, new_pos)
 
-    def can_capture(self, current_pos, new_pos):
+    def can_capture(self, current_pos: Point, new_pos: Point) -> bool:
         if self.color == "white":
             if (new_pos.x == current_pos.x + 1 and new_pos.y == current_pos.y + 1) or (new_pos.x == current_pos.x - 1 and new_pos.y == current_pos.y + 1):
                 return True
@@ -19,71 +21,71 @@ class Pawn:
     
 
 class Bishop(Pawn):
-    def __init__(self, color):
+    def __init__(self, color: str):
         super().__init__(color)
     
-    def can_move(self, current_pos, new_pos):
+    def can_move(self, current_pos: Point, new_pos: Point) -> bool:
         return is_moving_diagonally(current_pos, new_pos)
 
-    def can_capture(self, current_pos, new_pos):
+    def can_capture(self, current_pos: Point, new_pos: Point) -> bool:
         return is_moving_diagonally(current_pos, new_pos)
     
 
 class Rook(Pawn):
-    def __init__(self, color):
+    def __init__(self, color: str):
         super().__init__(color)
 
-    def can_move(self, current_pos, new_pos):
+    def can_move(self, current_pos: Point, new_pos: Point) -> bool:
         return is_moving_sideways(current_pos, new_pos)
 
-    def can_capture(self, current_pos, new_pos):
+    def can_capture(self, current_pos: Point, new_pos: Point):
         pass
 
 
 class Knight(Pawn):
-    def __init__(self, color):
+    def __init__(self, color: str):
         super().__init__(color)
 
-    def can_move(self, current_pos, new_pos):
+    def can_move(self, current_pos: Point, new_pos: Point) -> bool:
         if (abs(new_pos.x - current_pos.x) == 2 and abs(new_pos.y - current_pos.y) == 1) or (abs(new_pos.x - current_pos.x) == 1 and abs(new_pos.y - current_pos.y) == 2):
             return True
         return False
 
-    def can_capture(self, current_pos, new_pos):
+    def can_capture(self, current_pos: Point, new_pos: Point):
         pass
 
 
 class Queen(Pawn):
-    def __init__(self, color):
+    def __init__(self, color: str):
         super().__init__(color)
 
-    def can_move(self, current_pos, new_pos):
-        return is_moving_diagonally(current_pos, new_pos) or is_moving_forward(current_pos, new_pos) or is_moving_sideways(current_pos, new_pos)
+    def can_move(self, current_pos: Point, new_pos: Point) -> bool:
+        return is_moving_diagonally(current_pos, new_pos) or is_moving_forward(self.color, current_pos, new_pos) or is_moving_sideways(current_pos, new_pos)
 
-    def can_capture(self, current_pos, new_pos):
+    def can_capture(self, current_pos: Point, new_pos: Point):
         pass
 
 
 class King(Pawn):
-    def __init__(self, color):
+    def __init__(self, color: str):
         super().__init__(color)
 
-    def can_move(self, current_pos, new_pos):
+    def can_move(self, current_pos: Point, new_pos: Point) -> bool:
         if abs(new_pos.x - current_pos.x) <= 1 and abs(new_pos.y - current_pos.y) <= 1:
             return True
         return False
 
-    def can_capture(self, current_pos, new_pos):
+    def can_capture(self, current_pos: Point, new_pos: Point):
         pass
 
 
-def is_moving_diagonally(current_pos, new_pos):
+def is_moving_diagonally(current_pos: Point, new_pos: Point):
     if abs(new_pos.x - current_pos.x) == abs(new_pos.y - current_pos.y):
         return True
     return False
 
 
-def is_moving_forward_one_square(color, current_pos, new_pos):
+def is_moving_forward_one_square(color: str, current_pos: Point, new_pos: Point) -> bool:
     if color == "white":
         if new_pos.y == current_pos.y + 1 and new_pos.x == current_pos.x:
             return True
@@ -93,7 +95,7 @@ def is_moving_forward_one_square(color, current_pos, new_pos):
     return False
 
 
-def is_moving_forward_two_squares(color, current_pos, new_pos):
+def is_moving_forward_two_squares(color: str, current_pos: Point, new_pos: Point) -> bool:
     if color == "white":
         if current_pos.y == 1 and new_pos.y == current_pos.y + 2 and new_pos.x == current_pos.x:
             return True
@@ -103,7 +105,7 @@ def is_moving_forward_two_squares(color, current_pos, new_pos):
     return False
 
 
-def is_moving_forward(color, current_pos, new_pos):
+def is_moving_forward(color: str, current_pos: Point, new_pos: Point) -> bool:
     if color == "white":
         if new_pos.y > current_pos.y and new_pos.x == current_pos.x:
             return True
@@ -113,7 +115,7 @@ def is_moving_forward(color, current_pos, new_pos):
     return False
 
 
-def is_moving_sideways(current_pos, new_pos):
+def is_moving_sideways(current_pos: Point, new_pos: Point) -> bool:
     if new_pos.y == current_pos.y and new_pos.x != current_pos.x:
         return True
     return False
--- test/test_pawns.py
+++ test/test_pawns.py
@@ -14,7 +14,7 @@ class TestPawn(unittest.TestCase):
         ("black", Point(1, 6), Point(1, 4), True),
     ])
 
-    def test_pawn_can_take_one_or_two_steps_forward(self, color, current_pos, new_pos, expected):
+    def test_pawn_can_take_one_or_two_steps_forward(self, color: str, current_pos: Point, new_pos: Point, expected: bool):
         self.assertEqual(Pawn(color).can_move(current_pos, new_pos), expected)
 
     @parameterized.expand([
@@ -35,7 +35,7 @@ class TestPawn(unittest.TestCase):
     ])
 
     def test_pawn_cannot_move_diagonally_or_sideways_or_take_more_steps_than_two(
-        self, color, current_pos, new_pos, expected):
+        self, color: str, current_pos: Point, new_pos: Point, expected: bool):
         self.assertEqual(Pawn(color).can_move(current_pos, new_pos), expected)
 
     def test_white_pawn_cannot_move_forward_two_squares_after_moving(self):
@@ -49,6 +49,7 @@ class TestPawn(unittest.TestCase):
         new_pos = Point(1, 5)
         self.assertTrue(Pawn("black").can_move(current_pos, new_pos))
         self.assertFalse(Pawn("black").can_move(new_pos, Point(1, 3)))
+
 
 class TestBishop(unittest.TestCase):
     pass
