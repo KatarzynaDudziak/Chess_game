477bb19e99be539f8d6cbec24fc1f10194d214cd
Kasia <katarzynadudziak94@gmail.com>
2025-04-09 14:48:26 +0200

    Added pieces, squares and pieces movement to the window game

--- game.py
+++ game.py
@@ -1,10 +1,11 @@
 import pygame
 
-from board import Board
+from chessgame import *
 from pawns import *
 
 pygame.init()
-board = Board(8, 8)
+chessgame = ChessGame()
+board = chessgame.board
 screen = pygame.display.set_mode((904, 904))
 
 def load_and_scale_image(image_path, size=(104, 104)):
@@ -42,26 +43,47 @@ pieces = {
     BlackKing: black_king
 }
 
+def draw_board():
+    for row in range(8):
+        for column in range(8):
+            if (row + column) % 2 == 0:
+                screen.blit(dark_square, (28 + row * 106, 904 - 28 - 106 - column * 106))
+            else:
+                screen.blit(light_square, (28 + row * 106, 904 - 28 - 106 - column * 106))
+
+def draw_pieces():
+    for pawn_type, point in board.white_pawns:
+        screen.blit(pieces[pawn_type], (28 + point.x * 106, 904 - 28 - 106 - point.y * 106))
+    for pawn_type, point in board.black_pawns:
+        screen.blit(pieces[pawn_type], (28 + point.x * 106, 904 - 28 - 106 - point.y * 106))
+
+def convert_to_point(x, y):
+    return Point(abs((x - 28)) // 106, abs((904 - 28 - y)) // 106)
 
 running = True
 while running:
+    
+    screen.blit(bg, (0, 0))
+    draw_board()
+    draw_pieces()
     for event in pygame.event.get():
-        screen.blit(bg, (0, 0))
-
-        for row in range(8):
-            for column in range(8):
-                if (row + column) % 2 == 0:
-                    screen.blit(dark_square, (28 + row * 106, 770 - column * 106))
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            piece_pos = pygame.mouse.get_pos()
+            x, y = piece_pos
+            point = convert_to_point(x, y)
+            piece = board.get_piece(point)
+            if piece:
+                print(f"The piece {piece.__class__.__name__} and the position {point.x} {point.y}")
+        elif event.type == pygame.MOUSEBUTTONUP:
+            move = pygame.mouse.get_pos()
+            x, y = move
+            new_point = convert_to_point(x, y)
+            if chessgame.move_piece(point, new_point):
+                if type(piece) in pieces:
+                    screen.blit(pieces[type(piece)], (28 + new_point.x * 106, 904 - 28 - 106 - new_point.y * 106))
                 else:
-                    screen.blit(light_square, (28 + row * 106, 770 - column * 106))
-                    
-        for pawn_type, point in board.white_pawns:
-            screen.blit(pieces[pawn_type], (28 + point.x * 106, 770 - point.y * 106))
-        for pawn_type, point in board.black_pawns:
-            screen.blit(pieces[pawn_type], (28 + point.x * 106, 770 - point.y * 106))
-            
-
+                    print(f"Error: Piece type {type(piece)} not found in pieces dictionary.")
         if event.type == pygame.QUIT:
             running = False
-    pygame.display.flip()
+    pygame.display.update()
 pygame.quit()
