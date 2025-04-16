dad15ae8c3b6d76c13a0773eea3987b058cf39b4
Kasia <katarzynadudziak94@gmail.com>
2025-04-16 14:30:51 +0200

    Added Game class and refactored code

--- game.py
+++ game.py
@@ -4,127 +4,150 @@ from chessgame import *
 from pawns import *
 from check_exception import CheckException
 
-pygame.init()
-chessgame = ChessGame()
-board = chessgame.board
-width = 904
-height = 904
-screen = pygame.display.set_mode((904, 904))
-font = pygame.font.Font(None, 48)
 
+class Game:
+    def __init__(self) -> None:
+        pygame.init()
+        self.chessgame = ChessGame()
+        self.board = self.chessgame.board
+        self.board_width = 904
+        self.board_height = 904
+        self.square_width = 106
+        self.square_height = 106
+        self.piece_size = (104, 104)
+        self.frame_width = 28
+        self.red_text_color = (255, 0, 0)
+        self.screen = pygame.display.set_mode((self.board_width, self.board_height))
+        self.font = pygame.font.Font(None, 48)
+        self.selected_point = None
+        self.selected_piece = None
 
-def load_and_scale_image(image_path, size=(104, 104)):
-    image = pygame.image.load(image_path)
-    return pygame.transform.scale(image, size)
+        self.bg = self.load_and_scale_image("images/board_images/board.png", (self.board_width, self.board_height))
+        self.dark_square = self.load_and_scale_image("images/board_images/squareB.png", (self.square_width, self.square_height))
+        self.light_square = self.load_and_scale_image("images/board_images/squareW.png", (self.square_width, self.square_height))
+        self.white_pawn = self.load_and_scale_image("images/pawnW2.png")
+        self.white_rook = self.load_and_scale_image("images/rookW2.png")
+        self.white_knight = self.load_and_scale_image("images/knightW2.png")
+        self.white_bishop = self.load_and_scale_image("images/bishopW2.png")
+        self.white_queen = self.load_and_scale_image("images/queenW2.png")
+        self.white_king = self.load_and_scale_image("images/kingW2.png")
+        self.black_pawn = self.load_and_scale_image("images/pawnB2.png")
+        self.black_rook = self.load_and_scale_image("images/rookB2.png")
+        self.black_knight = self.load_and_scale_image("images/knightB2.png")
+        self.black_bishop = self.load_and_scale_image("images/bishopB2.png")
+        self.black_queen = self.load_and_scale_image("images/queenB2.png") 
+        self.black_king = self.load_and_scale_image("images/kingB2.png")
 
-bg = load_and_scale_image("images/board_images/board.png", (904, 904))
-dark_square = load_and_scale_image("images/board_images/squareB.png", (106, 106))
-light_square = load_and_scale_image("images/board_images/squareW.png", (106, 106))
-white_pawn = load_and_scale_image("images/pawnW2.png")
-white_rook = load_and_scale_image("images/rookW2.png")
-white_knight = load_and_scale_image("images/knightW2.png")
-white_bishop = load_and_scale_image("images/bishopW2.png")
-white_queen = load_and_scale_image("images/queenW2.png")
-white_king = load_and_scale_image("images/kingW2.png")
-black_pawn = load_and_scale_image("images/pawnB2.png")
-black_rook = load_and_scale_image("images/rookB2.png")
-black_knight = load_and_scale_image("images/knightB2.png")
-black_bishop = load_and_scale_image("images/bishopB2.png")
-black_queen = load_and_scale_image("images/queenB2.png")
-black_king = load_and_scale_image("images/kingB2.png")
+        self.pieces = {
+            WhitePawn: self.white_pawn,
+            WhiteRook: self.white_rook,
+            WhiteKnight: self.white_knight,
+            WhiteBishop: self.white_bishop,
+            WhiteQueen: self.white_queen,
+            WhiteKing: self.white_king,
+            BlackPawn: self.black_pawn,
+            BlackRook: self.black_rook,
+            BlackKnight: self.black_knight,
+            BlackBishop: self.black_bishop,
+            BlackQueen: self.black_queen,
+            BlackKing: self.black_king
+        }
 
-pieces = {
-    WhitePawn: white_pawn,
-    WhiteRook: white_rook,
-    WhiteKnight: white_knight,
-    WhiteBishop: white_bishop,
-    WhiteQueen: white_queen,
-    WhiteKing: white_king,
-    BlackPawn: black_pawn,
-    BlackRook: black_rook,
-    BlackKnight: black_knight,
-    BlackBishop: black_bishop,
-    BlackQueen: black_queen,
-    BlackKing: black_king
-}
+    def load_and_scale_image(self, image_path, size=None):
+        if size is None:
+            size = self.piece_size
+        image = pygame.image.load(image_path)
+        return pygame.transform.scale(image, size)
 
+    def draw(self):
+        for row in range(8):
+            for column in range(8):
+                square = self.dark_square if (row + column) % 2 == 0 else self.light_square
+                self.screen.blit(square, (self.frame_width + row * self.square_width, \
+                                                         self.board_height - self.frame_width - self.square_height - column * self.square_height))
+              
+        for pawn_type, point in self.board.white_pawns + self.board.black_pawns:
+            self.screen.blit(self.pieces[pawn_type], (self.frame_width + point.x * self.square_width, \
+                                                       self.board_height - self.frame_width - self.square_height - point.y * self.square_height))
+       
+    def convert_to_point(self, x, y):
+        return Point(abs((x - self.frame_width)) // self.square_width, abs((self.board_height - self.frame_width - y)) // self.square_height)
 
-def draw_board():
-    for row in range(8):
-        for column in range(8):
-            if (row + column) % 2 == 0:
-                screen.blit(dark_square, (28 + row * 106, 904 - 28 - 106 - column * 106))
-            else:
-                screen.blit(light_square, (28 + row * 106, 904 - 28 - 106 - column * 106))
+    def update_board_and_pieces(self):
+        self.draw()
 
+    def handle_check_exception(self, ex):
+        self.update_board_and_pieces()
+        self.show_check_message(ex)
 
-def draw_pieces():
-    for pawn_type, point in board.white_pawns:
-        screen.blit(pieces[pawn_type], (28 + point.x * 106, 904 - 28 - 106 - point.y * 106))
-    for pawn_type, point in board.black_pawns:
-        screen.blit(pieces[pawn_type], (28 + point.x * 106, 904 - 28 - 106 - point.y * 106))
+    def handle_checkmate_exception(self, ex):
+        self.update_board_and_pieces()
+        self.show_checkmate_message(ex)
 
-
-def convert_to_point(x, y):
-    return Point(abs((x - 28)) // 106, abs((904 - 28 - y)) // 106)
-
-
-def update_board_and_pieces():
-    draw_board()
-    draw_pieces()
-
-
-def show_checkmate_message():
-    update_board_and_pieces()
-    game_over = font.render("Checkmate! Game over!", True, (255, 0, 0), None)
-    game_over_rect = game_over.get_rect(center=(width//2, height//2))
-    screen.blit(game_over, game_over_rect)
-    pygame.display.update()
-    pygame.time.wait(3000)
-    logger.info(f"GameOverException occurred: {ex}")
-
-
-def show_check_message():
-    update_board_and_pieces()
-    check = font.render("Check!", True, (255, 0, 0), None)
-    check_rect = check.get_rect(center=(width//2, height//2))
-    screen.blit(check, check_rect)
-    pygame.display.update()
-    pygame.time.wait(1000)
-    logger.info(f"CheckException occurred: {ex}")
-
-
-running = True
-while running:
-    screen.blit(bg, (0, 0))
-    update_board_and_pieces()
-    try:
-        for event in pygame.event.get():
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                piece_pos = pygame.mouse.get_pos()
-                x, y = piece_pos
-                point = convert_to_point(x, y)
-                piece = board.get_piece_at_the_position(point)
-                if piece:
-                    print(f"The piece {piece.__class__.__name__} and the position {point.x} {point.y}")
-            elif event.type == pygame.MOUSEBUTTONUP:
-                move = pygame.mouse.get_pos()
-                x, y = move
-                new_point = convert_to_point(x, y)
-                try:
-                    if chessgame.move_piece(point, new_point):
-                        if type(piece) in pieces:
-                            screen.blit(pieces[type(piece)], (28 + new_point.x * 106, 904 - 28 - 106 - new_point.y * 106))
-                        else:
-                            print(f"Error: Piece type {type(piece)} not found in pieces dictionary.")
-                except GameOverException as ex:
-                    show_checkmate_message()
-                    running = False
-                except CheckException as ex:
-                    show_check_message()
-            if event.type == pygame.QUIT:
-                running = False
+    def show_checkmate_message(self, ex):
+        game_over = self.font.render("Checkmate! Game over!", True, self.red_text_color, None)
+        game_over_rect = game_over.get_rect(center=(self.board_width//2, self.board_height//2))
+        self.screen.blit(game_over, game_over_rect)
         pygame.display.update()
-    except Exception as ex:
-        logger.error(f"An unexpected error occurred: {ex}")
-pygame.quit()
+        pygame.time.wait(3000)
+        logger.info(f"GameOverException occurred: {ex}")
+
+    def show_check_message(self, ex):
+        check = self.font.render("Check!", True, self.red_text_color, None)
+        check_rect = check.get_rect(center=(self.board_width//2, self.board_height//2))
+        self.screen.blit(check, check_rect)
+        pygame.display.update()
+        pygame.time.wait(1000)
+        logger.info(f"CheckException occurred: {ex}")
+
+    def convert_pixel_point_to_board_point(self):
+        piece_pos = pygame.mouse.get_pos()
+        x, y = piece_pos
+        return self.convert_to_point(x, y)
+
+    def move_piece(self, piece, new_point):
+        if type(piece) in self.pieces:
+            self.screen.blit(self.pieces[type(piece)], (self.frame_width + new_point.x * self.square_width, \
+                                                         self.board_height - self.frame_width - self.square_height - new_point.y * self.square_height))
+        else:
+            print(f"Error: Piece type {type(piece)} not found in pieces dictionary.")
+
+    def handle_input(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            self.selected_point = self.convert_pixel_point_to_board_point()
+            self.selected_piece = self.board.get_piece_at_the_position(self.selected_point)
+            if self.selected_piece:
+                print(f"The piece {self.selected_piece.__class__.__name__} and the position {self.selected_point.x} {self.selected_point.y}")
+        elif event.type == pygame.MOUSEBUTTONUP:
+            new_point = self.convert_pixel_point_to_board_point()
+            try:
+                if self.chessgame.move_piece(self.selected_point, new_point):
+                    self.move_piece(self.selected_piece, new_point) 
+            except GameOverException as ex:
+                self.handle_checkmate_exception(ex)
+                return False
+            except CheckException as ex:
+                self.handle_check_exception(ex)
+        elif event.type == pygame.QUIT:
+            return False
+        return True
+
+
+def main():
+    game = Game()
+    running = True
+    while running:
+        game.screen.blit(game.bg, (0, 0))
+        game.update_board_and_pieces()
+        try:
+            for event in pygame.event.get():
+                if not game.handle_input(event):
+                    running = False
+            pygame.display.update()
+        except Exception as ex:
+            logger.error(f"An unexpected error occurred: {ex}")
+    pygame.quit()
+
+
+if __name__=="__main__":
+    main()
